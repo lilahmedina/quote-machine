c68bd4a804ca0ff31c1a836928c64687d946e388
Delilah Medina <67711334+lilahmedina@users.noreply.github.com>
2024-04-13 16:22:18 +0000

    complete FCC tasks

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ const getRandomQuote = (): Quote => {
 
 function App() {
   const [quote, setQuote] = useState<Quote>(getRandomQuote());
+
+  //for button to change quote
   const changeQuote = () => {
     setQuote(getRandomQuote());
   }
@@ -30,18 +32,18 @@ function App() {
         <FaQuoteRight size="30" style={{marginLeft: "10px"}} />
         <h4 id="author">- {quote.author}</h4>
       </div>
+
       <div className="buttons">
-        <a href="https://twitter.com/intent/post?hashtags=quotes&related=freecodecamp&text=%22First%2C%20have%20a%20definite%2C%20clear%20practical%20ideal%3B%20a%20goal%2C%20an%20objective.%20Second%2C%20have%20the%20necessary%20means%20to%20achieve%20your%20ends%3B%20wisdom%2C%20money%2C%20materials%2C%20and%20methods.%20Third%2C%20adjust%20all%20your%20means%20to%20that%20end.%22%20Aristotle" 
+        <a href="https://twitter.com/intent/tweet" 
         target='_blank'
         id="tweet-quote"
         style={{
           backgroundColor: "lightblue",
           marginRight: "15px",
-
-
         }} > <FaTwitter color="vanilla"></FaTwitter> 
         </a>
-        <button id="newQuote" onClick={changeQuote}>Change Quote</button>
+        
+        <button id="new-quote" onClick={changeQuote}>Change Quote</button>
       </div>
     </div>
   </div>
